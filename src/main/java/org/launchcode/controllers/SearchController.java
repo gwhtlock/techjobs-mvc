96991d5148de1df4adf62f49685c9941120695c4
Aaron Whitlock <whitlock.aaron@yahoo.com>
2019-06-20 15:06:14 -0500

    search function works for column but not for all columns

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -16,12 +17,42 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "")
+
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
+
+
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+
+    @RequestMapping(value ="/results", method = RequestMethod.POST)
+    public String search(Model model,@RequestParam  String searchType, String searchTerm){
+
+        model.addAttribute("columns", ListController.columnChoices);
+
+        searchType = searchType.toLowerCase();
+        searchTerm = searchTerm.toLowerCase();
+
+        ArrayList<HashMap<String, String>> jobsFound = new ArrayList<>();
+
+        if(searchType == "all"){
+
+            jobsFound = JobData.findByValue(searchTerm);
+
+        }else{
+            jobsFound = JobData.findByColumnAndValue(searchType,searchTerm);
+
+        }
+        model.addAttribute("searchType",searchType);
+        model.addAttribute("searchTerm",searchTerm);
+        model.addAttribute("jobsFound",jobsFound);
+
+        return "search";
+    }
 
 }
